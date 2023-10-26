5d374288849f040287306186163baa613c9aa0ad
AlefSillva <alefsilva0295@gmail.com>
2023-10-25 22:57:18 -0300

    update

---
+++ swagger/swaggerDefinition.js
@@ -0,0 +1,57 @@
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+      title: 'API de Alunos da Escola',
+      version: '1.0.0',
+      description: 'Documentação da API de Alunos da Escola',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000', // URL base da API 
+        description: 'Servidor de Desenvolvimento',
+      },
+      
+    ],
+    paths: {
+      '/alunos': {
+        get: {
+          summary: 'Listar alunos',
+          description: 'Retorna uma lista de todos os alunos da escola.',
+          responses: {
+            '200': {
+              description: 'Lista de alunos retornada com sucesso.',
+              content: {
+                'application/json': {
+                  example: [
+                    {
+                      id: 1,
+                      nome: 'João',
+                      idade: 18,
+                      notaPrimeiroSemestre: 8.5,
+                      notaSegundoSemestre: 9.0,
+                      nomeProfessor: 'Prof. Silva',
+                      numeroSala: 101,
+                    },
+                    {
+                      id: 2,
+                      nome: 'Maria',
+                      idade: 17,
+                      notaPrimeiroSemestre: 9.2,
+                      notaSegundoSemestre: 8.8,
+                      nomeProfessor: 'Prof. Santos',
+                      numeroSala: 102,
+                    },
+                  ],
+                },
+              },
+            },
+            
+          },
+        },
+      },
+      
+    },
+  };
+  
+  module.exports = swaggerDefinition;
+  
