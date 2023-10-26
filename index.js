9734b5e7e217757df22df33de5a6b6591ba4005e
AlefSillva <alefsilva0295@gmail.com>
2023-10-26 00:08:56 -0300

    delete coments

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000; // Use a porta 3000, a menos que a variável de ambiente PORT esteja definida
+const port = process.env.PORT || 3000; 
 
 // Configuração do Swagger
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDefinition = require('./swagger/swaggerDefinition'); // Certifique-se de que o caminho está correto
+const swaggerDefinition = require('./swagger/swaggerDefinition'); 
 
 // Definir a documentação Swagger
 const options = {
   swaggerDefinition,
-  apis: ['./routes/*.js'], // Certifique-se de que seus arquivos de rota estejam no diretório correto
+  apis: ['./routes/*.js'],
 };
 const swaggerSpec = swaggerJSDoc(options);
 
